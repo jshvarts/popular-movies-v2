bfbc0a4a51bf47ab2eeb12e403b78c6f349d46ef
James Shvarts <james.shvarts@viacom.com>
2015-12-08 18:57:45 -0500

    initial commit

---
+++ app/src/main/java/com/jshvarts/popularmovies/MovieListFragment.java
@@ -0,0 +1,21 @@
+package com.jshvarts.popularmovies;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Movie list fragment responsible for loading data for grid layout.
+ */
+public class MovieListFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_movie_list, container, false);
+
+        return view;
+    }
+}
