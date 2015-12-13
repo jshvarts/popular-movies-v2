0f900ca80952fab52e938d71f2338e41a3f3624a
James Shvarts <jshvarts@gmail.com>
2015-12-13 12:02:27 -0500

    added action bar to the settings screen and updated README

--- app/src/main/java/com/jshvarts/popularmovies/ui/MovieDetailActivity.java
+++ app/src/main/java/com/jshvarts/popularmovies/ui/MovieDetailActivity.java
@@ -1,12 +1,8 @@
 package com.jshvarts.popularmovies.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
 import com.google.common.base.Preconditions;
 import com.jshvarts.popularmovies.R;
@@ -15,7 +11,7 @@ import com.jshvarts.popularmovies.application.MovieDetailsRequestedEvent;
 import de.greenrobot.event.EventBus;
 
 /**
- * Movie detail activity
+ * Movie detail activity. Note: there is no need to add the options menu in this activity
  */
 public class MovieDetailActivity extends AppCompatActivity {
 
@@ -32,22 +28,5 @@ public class MovieDetailActivity extends AppCompatActivity {
         Preconditions.checkState(!TextUtils.isEmpty(id), "valid id in intent is required");
 
         EventBus.getDefault().post(new MovieDetailsRequestedEvent(id));
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
