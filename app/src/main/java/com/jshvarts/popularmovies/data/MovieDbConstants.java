c53fc8aa0b7f3ac015b132b7d137119ad1b037d7
James Shvarts <james.shvarts@viacom.com>
2015-12-30 09:56:07 -0500

    retrofit code clean up https://github.com/jshvarts/popular-movies-v2/issues/2

---
+++ app/src/main/java/com/jshvarts/popularmovies/data/MovieDbConstants.java
@@ -0,0 +1,8 @@
+package com.jshvarts.popularmovies.data;
+
+/**
+ * Re-usable constants across MovieDb data-access components.
+ */
+public class MovieDbConstants {
+    static final String BASE_API_URL = "http://api.themoviedb.org";
+}
